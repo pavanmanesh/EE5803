9c07b6e0b723ccdb531a86349349a1ef851fee68
pavanmanesh <70571595+pavanmanesh@users.noreply.github.com>
2022-02-05 18:01:23 +0530

    Add files via upload

---
+++ assign2/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+int n=13;
+void setup() {
+  pinMode(n,OUTPUT);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  //digitalWrite(n,HIGH);
+  //delay(3000);
+  digitalWrite(n,LOW);
+  //delay(3000);
+  // put your main code here, to run repeatedly:
+}
+
